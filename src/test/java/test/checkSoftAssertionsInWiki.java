720806831854c8d7f00300f4068c258e00588b34
artl <artemlarichev.v@gmail.com>
2025-02-17 21:29:12 +0100

    added testsuccessfulSearchJUnit5Test

---
+++ src/test/java/test/checkSoftAssertionsInWiki.java
@@ -0,0 +1,53 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class checkSoftAssertionsInWiki {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulSearchJUnit5Test() {
+
+        open("/selenide/selenide");
+
+
+        $("#wiki-tab ").click();
+
+        $(".js-wiki-more-pages-link ").click();
+
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+
+        $(byText("SoftAssertions")).click();
+
+        $(byTagAndText("h4", "3. Using JUnit5 extend test class:")).shouldBe(visible);
+
+        $("div:nth-child(19)").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+
+    }
+}
